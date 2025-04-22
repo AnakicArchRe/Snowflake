e34495677a823bfd27806b635fe7b5162f10088a
AnakicArchRe <anakicalfirevic@archre.com>
2025-04-22 13:29:25 +0200

    Resolve_scenario_data: exclude retros that don't interact with selected portfolios

--- ECONOMIC_MODEL_COMPUTED/Procedures/RESOLVE_SCENARIO_DATA().sql
+++ ECONOMIC_MODEL_COMPUTED/Procedures/RESOLVE_SCENARIO_DATA().sql
@@ -45,69 +45,7 @@ begin
         order by 
             scenarioid, partid;
 
-    -- scenario depth (required for processing blocks in order, i.e. parent before child)
-    create or replace table economic_model_scenario.scenario_meta as 
-        select distinct
-            partid as scenarioid, depth
-        from 
-            economic_model_scenario.scenario_parts;
-
-    -- retrocontract
-    create or replace table economic_model_computed.retrocontract_scenario as
-        with scenarioRetroContracts as (
-            select 
-                distinct scenarioid, retrocontractid
-            from 
-                economic_model_scenario.scenario s
-                cross join table(split_to_table(coalesce(s.analysis_retrocontractids, ''), ',')) t
-                inner join economic_model_revoext.retrocontract r on trim(t.value) = r.retrocontractid
-            where 
-                s.isactive = 1
-        )
-        , witOrigAndResolvedValues as (
-            select distinct
-                sp.scenarioid, 
-                rc.* exclude (commissiononnetpremium, level, isactive, profitcommissionpctofprofit, ReinsuranceBrokerageOnNetPremium, ReinsuranceExpensesOnCededCapital, ReinsuranceExpensesOnCededPremium),
-                coalesce(last_value(rc_o.isactive) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc), rc.isactive) isactive,
-                coalesce(last_value(rc_o.level) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc), rc.level) level,
-                coalesce(last_value(rc_o.commission) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc), rc.commissiononnetpremium) commissiononnetpremium,
-                coalesce(last_value(rc_o.profitcommission) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc), rc.profitcommissionpctofprofit) profitcommissionpctofprofit,
-                coalesce(last_value(rc_o.ReinsuranceBrokerageOnNetPremium) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc), rc.ReinsuranceBrokerageOnNetPremium) ReinsuranceBrokerageOnNetPremium,
-                coalesce(last_value(rc_o.ReinsuranceExpensesOnCededCapital) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc), rc.ReinsuranceExpensesOnCededCapital) ReinsuranceExpensesOnCededCapital,
-                coalesce(last_value(rc_o.ReinsuranceExpensesOnCededPremium) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc), rc.ReinsuranceExpensesOnCededPremium) ReinsuranceExpensesOnCededPremium,
-                last_value(case when irc.retrocontractid is null then false else true end) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc) includeInAnalysis,
-                last_value(case when rc_o.retrocontractid is null then false else true end) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc) HasOverride,
-                last_value(rc_o.targetcollateralcalculated) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc) TargetCollateralCalculated,
-
-                rc.isactive as isactive_original,
-                rc.level as level_original,
-                rc.commissiononnetpremium as commissiononnetpremium_original,
-                rc.profitcommissionpctofprofit as profitcommissionpctofprofit_original,
-                rc.ReinsuranceBrokerageOnNetPremium as ReinsuranceBrokerageOnNetPremium_original,
-                rc.ReinsuranceExpensesOnCededCapital as ReinsuranceExpensesOnCededCapital_original,
-                rc.ReinsuranceExpensesOnCededPremium as ReinsuranceExpensesOnCededPremium_original,
-            from
-                economic_model_revoext.retrocontract rc
-                cross join economic_model_scenario.scenario_parts sp
-                left outer join economic_model_scenario.retrocontract_override rc_o on sp.partid = rc_o.scenarioid and rc.retrocontractid = rc_o.retrocontractid
-                left outer join scenarioRetroContracts irc on irc.retrocontractid = rc.retrocontractid and irc.scenarioid = sp.partid
-        )
-        select
-            * exclude (isactive_original, level_original, commissiononnetpremium_original, profitcommissionpctofprofit_original, ReinsuranceBrokerageOnNetPremium_original, ReinsuranceExpensesOnCededCapital_original, ReinsuranceExpensesOnCededPremium_original),
-            economic_model_computed.concat_non_null(
-                economic_model_computed.compare_and_note(isactive, isactive_original, 'IsActive'),
-                economic_model_computed.compare_and_note(level, level_original, 'Level'),
-                economic_model_computed.compare_and_note(commissiononnetpremium, commissiononnetpremium_original, 'CommissionOnNetPremium'),
-                economic_model_computed.compare_and_note(profitcommissionpctofprofit, profitcommissionpctofprofit_original, 'ProfitCommissionPctOfProfit'),
-                economic_model_computed.compare_and_note(ReinsuranceBrokerageOnNetPremium, ReinsuranceBrokerageOnNetPremium_original, 'ReinsuranceBrokerageOnNetPremium'),
-                economic_model_computed.compare_and_note(ReinsuranceExpensesOnCededCapital, ReinsuranceExpensesOnCededCapital_original, 'ReinsuranceExpensesOnCededCapital'),
-                economic_model_computed.compare_and_note(ReinsuranceExpensesOnCededPremium, ReinsuranceExpensesOnCededPremium_original, 'ReinsuranceExpensesOnCededPremium')
-            ) AS notes
-        from 
-            witOrigAndResolvedValues
-        ;
-
-
+    
     -- unpivoted topupzone overrides
     create or replace table economic_model_scenario.topupzone_override_unpivoted as
         with productGroup as (
@@ -148,19 +86,21 @@ begin
             shareFactor is not null 
             or premiumFactor is not null
           ;
-
+    
+    -- need to know which portfolios are visible in which scenario, so we can filter out portlayers that aren't part of them
+    -- and retro entities that don't interact with them
+    create or replace temporary table economic_model_computed.portfolio_scenario as
+        select 
+            scenarioid, p.*
+        from 
+            economic_model_scenario.scenario s
+            cross join table(split_to_table(coalesce(s.analysis_PORTFOLIOIDS, ''), ',')) t
+            inner join economic_model_staging.portfolio p on trim(t.value) = p.portfolioid
+    ;
 
     -- portlayer
     create or replace table economic_model_computed.portLayer_scenario as
-        with scenarioPortfolios as (
-            select 
-                scenarioid, portfolioid
-            from 
-                economic_model_scenario.scenario s
-                cross join table(split_to_table(coalesce(s.analysis_PORTFOLIOIDS, ''), ',')) t
-                inner join economic_model_staging.portfolio p on trim(t.value) = p.portfolioid
-        )
-        , withResolvedAndOriginalValues as (
+        with withResolvedAndOriginalValues as (
             select distinct
                 sp.scenarioid, 
                 pl.* exclude (sharefactor, premiumfactor),
@@ -181,7 +121,7 @@ begin
                 economic_model_staging.portlayer pl
                 cross join economic_model_scenario.scenario_parts sp
                 -- limit to portfolios included in scenario (todo: parts and main scenario should have the same list of portfolios)
-                inner join scenarioPortfolios pf on pl.portfolioid = pf.portfolioid and pf.scenarioid = sp.scenarioid
+                inner join economic_model_computed.portfolio_scenario pf on pl.portfolioid = pf.portfolioid and pf.scenarioid = sp.scenarioid
                 left outer join economic_model_scenario.portlayer_override pl_o on sp.partid = pl_o.scenarioid and pl.portlayerid = pl_o.portlayerid
                 left outer join economic_model_scenario.topupzone_override_unpivoted tz_u_o on sp.partid = tz_u_o.scenarioid and pl.topupzoneid = tz_u_o.topupzoneid and tz_u_o.productgroup = pl.productgroup
         )
@@ -194,6 +134,75 @@ begin
         from 
             withResolvedAndOriginalValues
         ;
+
+    -- filter out retros that don't impact a scenario (don't interact with portfolios it contains)
+    create or replace temporary table economic_model_computed.cessionflag as 
+        select distinct
+            p.scenarioid, retrocontractid
+        from 
+            economic_model_computed.portfolio_scenario p
+            inner join economic_model_staging.portlayer pl on p.portfolioid = pl.portfolioid
+            inner join economic_model_staging.portlayerperiod per on pl.portlayerid = per.portlayerid
+            inner join economic_model_staging.retrotag t on per.periodid = t.periodid
+            inner join economic_model_staging.retroconfiguration rc on t.retroconfigurationid = rc.retroconfigurationid;
+
+    -- retrocontract
+    create or replace table economic_model_computed.retrocontract_scenario as
+        with scenarioRetroContracts as (
+            select 
+                distinct scenarioid, retrocontractid
+            from 
+                economic_model_scenario.scenario s
+                cross join table(split_to_table(coalesce(s.analysis_retrocontractids, ''), ',')) t
+                inner join economic_model_revoext.retrocontract r on trim(t.value) = r.retrocontractid
+            where 
+                s.isactive = 1
+        )
+        , witOrigAndResolvedValues as (
+            select distinct
+                sp.scenarioid, 
+                rc.* exclude (commissiononnetpremium, level, isactive, profitcommissionpctofprofit, ReinsuranceBrokerageOnNetPremium, ReinsuranceExpensesOnCededCapital, ReinsuranceExpensesOnCededPremium),
+                coalesce(last_value(rc_o.isactive) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc), rc.isactive) isactive,
+                coalesce(last_value(rc_o.level) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc), rc.level) level,
+                coalesce(last_value(rc_o.commission) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc), rc.commissiononnetpremium) commissiononnetpremium,
+                coalesce(last_value(rc_o.profitcommission) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc), rc.profitcommissionpctofprofit) profitcommissionpctofprofit,
+                coalesce(last_value(rc_o.ReinsuranceBrokerageOnNetPremium) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc), rc.ReinsuranceBrokerageOnNetPremium) ReinsuranceBrokerageOnNetPremium,
+                coalesce(last_value(rc_o.ReinsuranceExpensesOnCededCapital) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc), rc.ReinsuranceExpensesOnCededCapital) ReinsuranceExpensesOnCededCapital,
+                coalesce(last_value(rc_o.ReinsuranceExpensesOnCededPremium) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc), rc.ReinsuranceExpensesOnCededPremium) ReinsuranceExpensesOnCededPremium,
+                last_value(case when irc.retrocontractid is null then false else true end) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc) includeInAnalysis,
+                last_value(case when rc_o.retrocontractid is null then false else true end) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc) HasOverride,
+                last_value(rc_o.targetcollateralcalculated) ignore nulls over (partition by sp.scenarioid, rc.retrocontractid order by depth asc) TargetCollateralCalculated,
+
+                rc.isactive as isactive_original,
+                rc.level as level_original,
+                rc.commissiononnetpremium as commissiononnetpremium_original,
+                rc.profitcommissionpctofprofit as profitcommissionpctofprofit_original,
+                rc.ReinsuranceBrokerageOnNetPremium as ReinsuranceBrokerageOnNetPremium_original,
+                rc.ReinsuranceExpensesOnCededCapital as ReinsuranceExpensesOnCededCapital_original,
+                rc.ReinsuranceExpensesOnCededPremium as ReinsuranceExpensesOnCededPremium_original,
+            from
+                economic_model_revoext.retrocontract rc
+                cross join economic_model_scenario.scenario_parts sp
+                left outer join economic_model_scenario.retrocontract_override rc_o on sp.partid = rc_o.scenarioid and rc.retrocontractid = rc_o.retrocontractid
+                left outer join scenarioRetroContracts irc on irc.retrocontractid = rc.retrocontractid and irc.scenarioid = sp.partid
+                -- make sure the retro interacts with portfolios visible in the scenario
+                inner join economic_model_computed.cessionflag f on rc.retrocontractid = f.retrocontractid and sp.partid = f.scenarioid
+        )
+        select
+            * exclude (isactive_original, level_original, commissiononnetpremium_original, profitcommissionpctofprofit_original, ReinsuranceBrokerageOnNetPremium_original, ReinsuranceExpensesOnCededCapital_original, ReinsuranceExpensesOnCededPremium_original),
+            economic_model_computed.concat_non_null(
+                economic_model_computed.compare_and_note(isactive, isactive_original, 'IsActive'),
+                economic_model_computed.compare_and_note(level, level_original, 'Level'),
+                economic_model_computed.compare_and_note(commissiononnetpremium, commissiononnetpremium_original, 'CommissionOnNetPremium'),
+                economic_model_computed.compare_and_note(profitcommissionpctofprofit, profitcommissionpctofprofit_original, 'ProfitCommissionPctOfProfit'),
+                economic_model_computed.compare_and_note(ReinsuranceBrokerageOnNetPremium, ReinsuranceBrokerageOnNetPremium_original, 'ReinsuranceBrokerageOnNetPremium'),
+                economic_model_computed.compare_and_note(ReinsuranceExpensesOnCededCapital, ReinsuranceExpensesOnCededCapital_original, 'ReinsuranceExpensesOnCededCapital'),
+                economic_model_computed.compare_and_note(ReinsuranceExpensesOnCededPremium, ReinsuranceExpensesOnCededPremium_original, 'ReinsuranceExpensesOnCededPremium')
+            ) AS notes
+        from 
+            witOrigAndResolvedValues
+        ;
+
 
 
     -- retroconfiguration
@@ -209,6 +218,8 @@ begin
             economic_model_staging.retroconfiguration l
             cross join economic_model_scenario.scenario_parts sp
             left outer join economic_model_scenario.retroconfiguration_override l_o on sp.partid = l_o.scenarioid and l.retroconfigurationid = l_o.retroconfigurationid
+            -- make sure the retro interacts with portfolios visible in the scenario
+            inner join economic_model_computed.cessionflag f on l.retrocontractid = f.retrocontractid and sp.partid = f.scenarioid
         ;
 
     
@@ -228,6 +239,9 @@ begin
                         economic_model_staging.retroinvestmentleg l
                         cross join economic_model_scenario.scenario_parts sp
                         left outer join economic_model_scenario.retroinvestmentleg_override l_o on sp.partid = l_o.scenarioid and l.retroinvestmentlegid = l_o.retroinvestmentlegid
+                        -- make sure the retro interacts with portfolios visible in the scenario
+                        inner join economic_model_staging.retroconfiguration rc on l.retroconfigurationid = rc.retroconfigurationid
+                        inner join economic_model_computed.cessionflag f on rc.retrocontractid = f.retrocontractid and sp.partid = f.scenarioid
             )
             select 
                 * exclude (investmentsigned_original, investmentsignedamt_original),
