18c4cf330b05e27c203fab8d54966f99f93f8854
AnakicArchRe <anakicalfirevic@archre.com>
2025-05-07 15:22:19 +0200

    formatting changes caused by sync (nothing of interest here)

--- ECONOMIC_MODEL_COMPUTED/Procedures/PREPAREPOWERBIDATA(NUMBER).sql
+++ ECONOMIC_MODEL_COMPUTED/Procedures/PREPAREPOWERBIDATA(NUMBER).sql
@@ -1,8 +1,10 @@
-CREATE OR REPLACE PROCEDURE ECONOMIC_MODEL_COMPUTED.PREPAREPOWERBIDATA(scenarioId int)
+CREATE OR REPLACE PROCEDURE ECONOMIC_MODEL_COMPUTED.PREPAREPOWERBIDATA(SCENARIOID NUMBER)
 RETURNS NUMBER(38,0)
 LANGUAGE SQL
 AS
 begin
+
+    -- todo: check if it makes sense to filter by scenario (i.e. only generate ylt blocks for specified scenarios)
 
     -- calculate YLT blocks for use in PowerBI (subject YLT blocks for product development)
     call economic_model_computed.calculate_subjectblocksylt(:scenarioId);
@@ -13,4 +15,5 @@ begin
     -- calculate YLT blocks for use in PowerBI (subject YLT blocks for product development)
     call economic_model_computed.calculate_netblocksylt(:scenarioId);
 
-end;
+end
+;
