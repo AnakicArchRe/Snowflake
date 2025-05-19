f9a2690325093944c637bf802d1f1ef0836261bb
AnakicArchRe <anakicalfirevic@archre.com>
2025-05-19 14:57:46 +0200

    Reducing granularity of subjectylt
    We're never reading ylt data at the portlayer level in PowerBI, so
    I'm reducing the granularity to the minimial necessary level. This
    reduces the amount of data to be sent to PowerBI by about 70%.
    
    If needed, we can implement lower-granularity-level calculation in
    PowerBI, using the slow YELPT-based DAX approach. That way, we have the
    results quickly at retro contract level, and if we only need the ylt
    data for specific portlayers, we can get that too.

--- ECONOMIC_MODEL_COMPUTED/Procedures/CALCULATE_SUBJECTBLOCKSYLT(NUMBER).sql
+++ ECONOMIC_MODEL_COMPUTED/Procedures/CALCULATE_SUBJECTBLOCKSYLT(NUMBER).sql
@@ -21,53 +21,42 @@ BEGIN
 
     call economic_model_computed.clearscenariodatafromtable('subjectblockylt', :scenarioId);
     
-    insert into economic_model_computed.subjectblockylt (year, peril, lossviewgroup, portlayerid, retrocontractid, scenarioid, subjectLoss, subjectRP, subjectRB)
-        with cte as (
-            select
-                y.year,
-                y.peril,
-                y.lossviewgroup, 
-                pl.portlayerid, 
-                rcf.retrocontractid, 
-                rb.scenarioid,
-                coalesce(rcs.nonmodeledload, 1) * least(coalesce(rcs.climateload, 1), y.maxlossscalefactor) as scaleFactor,
-                round(sum(scaleFactor * exposedlimit * totalloss))  subjectLoss,
-                round(sum(scaleFactor * exposedrp * totalrp)) subjectRP,
-                round(sum(scaleFactor * exposedrp * totalrb)) subjectRB,
-                -- no longer using dynamic climateload in PowerBI, so no need for this piece of information
-                -- note: as a reminder, it indicates how much a layer's losses can grow before hitting the cap (limit)
-                -- y.maxlossscalefactor / scalefactor as maxlossscalefactor
-            from 
-                economic_model_computed.blockoperations_out rb
-                inner join economic_model_staging.retrotag t on rb.blockid = t.retroblockid
-                inner join economic_model_staging.retroconfiguration rcf on t.retroconfigurationid = rcf.retroconfigurationid
-                inner join economic_model_staging.portlayerperiod per on t.periodid = per.periodid
-                inner join economic_model_staging.yelpt y on per.yeltperiodid = y.yeltperiodid
-                inner join economic_model_computed.portlayer_scenario pl on per.portlayerid = pl.portlayerid and rb.scenarioid = pl.scenarioid
-                inner join economic_model_computed.retrocontract_scenario rcs on rcf.retrocontractid = rcs.retrocontractid and rcs.scenarioid = rb.scenarioid
-            where
-                // The reason for this is that SubjectYLT is per-retro, so there are many millions of rows. To reduce the amount of data sent to powerbi, we skip
-                // calculation of the subject ylt blocks that are outside the retros specified by the scenario. We do not check this flag for net and gross YLT blocks
-                // because they are not per-retro, so there are not as many row there.
-                rcs.includeinanalysis = true
-            group by
-                y.year, 
-                y.peril, 
-                y.lossviewgroup, 
-                // If we need the the ability to filter by period, rather than by portlayer in powerbi, we'll need to to keep the granularity at period-level.
-                // If so, we should replace portlayerid with periodid in the grouping and select list.
-                // todo: Check with PC.
-                pl.portlayerid, 
-                rcf.retrocontractid, 
-                rb.scenarioid,
-                y.maxlossscalefactor, 
-                rcs.nonmodeledload,
-                rcs.climateload
-        )
-        select 
-            * exclude(scaleFactor)
+    insert into economic_model_computed.subjectblockylt (year, peril, lossviewgroup, portfolioid, topupzoneid, facility, inceptionMonth, retrocontractid, scenarioid, subjectLoss, subjectRP, subjectRB)
+        select
+            y.year,
+            y.peril,
+            y.lossviewgroup,
+            pl.portfolioid, 
+            pl.topupzoneid,
+            pl.facility,
+            month(pl.inception) as inceptionMonth,
+            rcf.retrocontractid, 
+            rb.scenarioid,
+            round(sum(coalesce(rcs.nonmodeledload, 1) * least(coalesce(rcs.climateload, 1), y.maxlossscalefactor) * exposedlimit * totalloss)) subjectLoss,
+            round(sum(coalesce(rcs.nonmodeledload, 1) * least(coalesce(rcs.climateload, 1), y.maxlossscalefactor) * exposedrp * totalrp)) subjectRP,
+            round(sum(coalesce(rcs.nonmodeledload, 1) * least(coalesce(rcs.climateload, 1), y.maxlossscalefactor) * exposedrp * totalrb)) subjectRB
         from 
-            cte
+            economic_model_computed.blockoperations_out rb
+            inner join economic_model_staging.retrotag t on rb.blockid = t.retroblockid
+            inner join economic_model_staging.retroconfiguration rcf on t.retroconfigurationid = rcf.retroconfigurationid
+            inner join economic_model_staging.portlayerperiod per on t.periodid = per.periodid
+            inner join economic_model_staging.yelpt y on per.yeltperiodid = y.yeltperiodid
+            inner join economic_model_computed.portlayer_scenario pl on per.portlayerid = pl.portlayerid and rb.scenarioid = pl.scenarioid
+            inner join economic_model_computed.retrocontract_scenario rcs on rcf.retrocontractid = rcs.retrocontractid and rcs.scenarioid = rb.scenarioid
+        where
+            // The reason for this is that SubjectYLT is per-retro, so there are many millions of rows. To reduce the amount of data sent to powerbi, we skip
+            // calculation of the subject ylt blocks that are outside the retros specified by the scenario. We do not check this flag for net and gross YLT blocks
+            // because they are not per-retro, so there are not as many row there.
+            rcs.includeinanalysis = true
+        group by
+            y.year, 
+            y.peril, 
+            y.lossviewgroup, 
+            pl.portfolioid, 
+            pl.topupzoneid,
+            pl.facility,
+            rcf.retrocontractid, 
+            rb.scenarioid
         ;
 
     call economic_model_computed.clearscenariodatafromtable('subjectblock_seasonal_premium', :scenarioId);
@@ -85,5 +74,4 @@ BEGIN
             inner join economic_model_staging.seasonality se on se.yeltperiodid = per.yeltperiodid
     ;
 
-end
-;
+end;
