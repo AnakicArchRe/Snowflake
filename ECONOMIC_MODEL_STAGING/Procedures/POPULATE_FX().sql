44c71a04c2f636f9a2e4b53289223a2865eb3f8d
AnakicArchRe <anakicalfirevic@archre.com>
2025-06-05 10:40:27 +0200

    Fix for generated fx dates
    Layer dates go farther than submission.fxdate and we should accont for this to allow layers with future fxdates to get an fx value.
    
    This was causing portlayers_scenario rows to not appear because the join to the fx was failing.

--- ECONOMIC_MODEL_STAGING/Procedures/POPULATE_FX().sql
+++ ECONOMIC_MODEL_STAGING/Procedures/POPULATE_FX().sql
@@ -9,7 +9,12 @@ begin
             -- generate dates
             dates as (
                 select dateadd(day, seq4(), (select min (fxdate) from revo_bermuda.dbo.fxrate)) date from table (generator(rowcount => (select 10e3)))
-                where date <= (select max(fxdate) from economic_model_raw.submission)
+                where date <= 
+                    greatest
+                    (
+                        (select max(fxdate) from economic_model_raw.submission), 
+                        (select max(boundfxdate) from economic_model_raw.layer)
+                    )
             )
             -- get fx records
             , fxRows as (
