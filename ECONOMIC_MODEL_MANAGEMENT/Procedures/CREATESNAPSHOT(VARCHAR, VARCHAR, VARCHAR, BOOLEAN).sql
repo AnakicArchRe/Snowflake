19749dd9dce325a11c69fbe97ebf06ab0baaf17f
AnakicArchRe <anakicalfirevic@archre.com>
2025-06-11 10:08:29 +0200

    createsnapshot() proc - recording ddl

--- ECONOMIC_MODEL_MANAGEMENT/Procedures/CREATESNAPSHOT(VARCHAR, VARCHAR, VARCHAR, BOOLEAN).sql
+++ ECONOMIC_MODEL_MANAGEMENT/Procedures/CREATESNAPSHOT(VARCHAR, VARCHAR, VARCHAR, BOOLEAN).sql
@@ -6,13 +6,29 @@ DECLARE
     TABLE_NAME varchar;
     TABLE_SCHEMA varchar;
     x varchar;
+    ddl varchar;
     TBL_CURR CURSOR FOR 
         SELECT TABLE_SCHEMA, TABLE_NAME
         FROM INFORMATION_SCHEMA.TABLES 
-        WHERE TABLE_CATALOG = 'ACTUARIAL_ILS_POC' AND TABLE_SCHEMA = 'ECONOMIC_MODEL_RAW' AND TABLE_TYPE = 'BASE TABLE';
+        WHERE 
+            TABLE_CATALOG = 'ACTUARIAL_ILS_POC' 
+            AND TABLE_SCHEMA in ('ECONOMIC_MODEL_RAW', 'ECONOMIC_MODEL_REVOEXT', 'ECONOMIC_MODEL_SCENARIO', 'ECONOMIC_MODEL_MANAGEMENT')
+            AND TABLE_TYPE = 'BASE TABLE';
 BEGIN
 
-    begin transaction;
+    -- begin transaction;
+
+    -- todo: make schema names dynamic?
+    set ddl:= (select 
+        concat(
+                get_ddl('schema', 'economic_model_raw') || '\n',
+                get_ddl('schema', 'economic_model_revoext') || '\n',
+                get_ddl('schema', 'economic_model_scenario') || '\n',
+                get_ddl('schema', 'economic_model_staging') || '\n',
+                get_ddl('schema', 'economic_model_computed') || '\n',
+                get_ddl('schema', 'economic_model_management')
+        )
+    );
 
     // delete previous records with this snapshotid
     delete from economic_model_management.SnapshotInfo
@@ -23,10 +39,13 @@ BEGIN
     where snapshotid = :snapshotid;
 
     // insert record for this snapshotid
-    insert into economic_model_management.SnapshotInfo(snapshotid, description, createdby, createdon)
-    values (:snapshotid, :description, :createdby, current_timestamp());
+    insert into economic_model_management.SnapshotInfo(snapshotid, description, createdby, createdon, ddl)
+    values (:snapshotid, :description, :createdby, current_timestamp(), :ddl);
 
-    // remove any old data from previous snapshot with the same id
+
+    -- make sure to add "execute as caller" after "language sql" in the procedure
+    -- definition. My powershell cmdlets keeps stripping it out as it can't read the "execute as" parameter from metadata (todo: look into options for this)
+    -- // remove any old data from previous snapshot with the same id
     execute immediate 
         'REMOVE @economic_model_management.snapshots/' || :snapshotid || '/';
 
